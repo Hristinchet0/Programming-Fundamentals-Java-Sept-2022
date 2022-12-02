b231f802992a72840f80bd4aa5d2e281e37584b4
Hristinchet0 <bansko.sport@gmail.com>
2022-12-02 10:30:52 +0200

    add

---
+++ Maps Lambda and Stream API Lab/p04WordFilter.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class p04WordFilter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] wordsArr = Arrays.stream(scanner.nextLine().split(" "))
+                .filter(w -> w.length() % 2 == 0)
+                .toArray(String[]::new);
+
+        System.out.println(String.join(System.lineSeparator(), wordsArr));
+    }
+}
